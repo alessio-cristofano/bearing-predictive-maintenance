58291f1b55ff447fae74cebc9388ae8be1d318d5
Alessio Cristofano <alessio.cristofano@adlerpelzer.com>
2026-09-17 12:08:20 +0200

    defined bronze and silver pipelines

---
+++ src/features/silver_pipeline.py
@@ -0,0 +1,38 @@
+import polars as pl
+from pathlib import Path
+from schemas.dataset_specs import SPECS
+
+
+def silver_pipeline(id: int):
+    path_to_bronze_data: Path = Path(f"./data/bronze/set{id}_bronze.parquet")
+    bronze_data: pl.DataFrame = pl.read_parquet(path_to_bronze_data)
+
+    # Group by snapshot timestamp (1 second) and compute relevant metrics
+    ## Metrics to compute:
+    # Mean
+    # Standard Deviation
+    # RMS
+    # Peak-to-Peak
+    # Kurtosis
+
+    data_columns: list[str] = SPECS.get(id).column_names
+    aggr_exprs: list[pl.Expr] = []
+
+    for column in data_columns:
+        aggr_exprs.extend(
+            [
+                pl.mean(column).name.suffix("_mean"),
+                pl.std(column).name.suffix("_std"),
+                (((pl.col(column) ** 2).mean()) ** 0.5).name.suffix("_rms"),
+                (pl.max(column) - pl.min(column)).name.suffix("_p2p"),
+                (((pl.col(column) - pl.mean(column)) / pl.std(column)) ** 4)
+                .mean()
+                .name.suffix("_kurtosis"),
+                pl.count(column).name.suffix("_count"),
+            ]
+        )
+    silver_data: pl.DataFrame = bronze_data.group_by("snapshot_timestamp").agg(aggr_exprs)
+
+    print(f"Rows of the silver dataset: {silver_data.height}")
+    path_to_silver_data: Path = Path(f"./data/silver/set{id}_silver.parquet")
+    silver_data.write_parquet(path_to_silver_data)
--- src/ingestion/bronze_pipeline.py
+++ src/ingestion/bronze_pipeline.py
@@ -1,31 +1,47 @@
 import os
+import sys
 from dotenv import load_dotenv
 import polars as pl
 from ingestion.raw_loader import load_snapshot
 from ingestion.download import download
 from pathlib import Path
+from schemas.dataset_specs import DatasetSpec
+import numpy as np
 
-DATASET_MAPPING: list[dict] = [
-    {"relative_path": "1st_test/1st_test", "id": 1},
-    {"relative_path": "2nd_test/2nd_test", "id": 2},
-    {"relative_path": "3rd_test/3rd_test", "id": 3},
-]
-TARGET_ID: int = 2
 
-dataset: dict = next(d for d in DATASET_MAPPING if d["id"] == TARGET_ID)
-#
-load_dotenv()
-data_path: Path = Path(os.getenv("DATA_PATH"))
+def bronze_pipeline(mapping: list[dict], id: int):
+    dataset: dict = next(d for d in mapping if d["id"] == id)
+    #
+    load_dotenv()
+    data_path: Path = Path(os.getenv("DATA_PATH"))
 
-while not any(data_path.iterdir()):
-    data_path = download()
+    if not data_path.exists() or not any(data_path.iterdir()):
+        data_path = download()
 
-files: list[Path] = [
-    f for f in (data_path / dataset.get("relative_path")).iterdir() if not f.is_dir()
-]
-test: list[pl.DataFrame] = []
-final: pl.DataFrame = None
-for f in files:
-    test.append(load_snapshot(f, dataset.get("id")))
-final = pl.concat(test)
-print(final.height)
+    if data_path is None:
+        sys.exit("No Data Path found")
+
+    files: list[Path] = [
+        f for f in (data_path / dataset.get("relative_path")).iterdir() if not f.is_dir()
+    ]
+    sizes: np.ndarray = np.array(
+        [
+            os.path.getsize(f)
+            for f in (data_path / dataset.get("relative_path")).iterdir()
+            if not f.is_dir()
+        ]
+    )
+    single_files: list[pl.DataFrame] = []
+    for f in files:
+        single_files.append(load_snapshot(f, dataset.get("id")))
+    merged_files = pl.concat(single_files)
+
+    # Check that the concatenation worked
+    if merged_files.height != len(single_files) * DatasetSpec.get_rows():
+        sys.exit("Loss of data during dataframe concatenation")
+
+    path_to_bronze_data: Path = Path(f"./data/bronze/set{id}_bronze.parquet")
+    merged_files.write_parquet(path_to_bronze_data)
+
+    print(f"Size of parquet file: {os.path.getsize(path_to_bronze_data)}")
+    print(f"Size of raw dataset files: {sizes.sum()}")
