04fd1b78b017b16d15eb3cf325b44d518cd36087
Alessio Cristofano <alessio.cristofano@adlerpelzer.com>
2026-09-17 12:08:54 +0200

    added query analysis script

---
+++ src/analysis/query_silver.py
@@ -0,0 +1,22 @@
+import duckdb
+from pathlib import Path
+from schemas.dataset_specs import SPECS
+
+TARGET_ID = 2
+path_to_silver_data: Path = Path(f"./data/silver/set{TARGET_ID}_silver.parquet")
+if not path_to_silver_data.exists():
+    raise FileNotFoundError(f"{path_to_silver_data.name}: Provided path does not exists")
+
+data_columns: list[str] = SPECS.get(TARGET_ID).column_names
+
+TARGET_METRIC: str = "rms"
+QUERY_PAYLOAD: str = ",".join(
+    [f"max({col}_{TARGET_METRIC}) AS max_{col}_{TARGET_METRIC}" for col in data_columns]
+)
+QUERY: str = f"""SELECT {QUERY_PAYLOAD} FROM '{path_to_silver_data}'"""
+duckdb.sql(QUERY).show()
+
+QUERY: str = (
+    f"""SELECT snapshot_timestamp,b1_rms FROM '{path_to_silver_data}' ORDER BY b1_rms DESC LIMIT 5"""
+)
+duckdb.sql(QUERY).show()
